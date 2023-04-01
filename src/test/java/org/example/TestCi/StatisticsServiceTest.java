977e0f32ebee5b539691d2c3a6e7e29eb97270a2
Moon <Aliya0609@mail.ru>
2023-04-01 21:18:49 +0500

    Test new added

--- src/test/java/org/example/TestCi/StatisticsServiceTest.java
+++ src/test/java/org/example/TestCi/StatisticsServiceTest.java
@@ -10,8 +10,18 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+    }
+
+    @Test
+    void findMaxEqual() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
     }
